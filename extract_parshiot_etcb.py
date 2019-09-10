a469ecc34b5a070e9e13a3bf79b3e418d3a70c5a
Joshua Waxman <joshwaxman@gmail.com>
2019-09-10 06:21:53 -0400

    started implementing versewise Leitwort detection

--- extract_parshiot_etcb.py
+++ extract_parshiot_etcb.py
@@ -2,6 +2,7 @@
 # sept 10, 2019
 from pymongo import MongoClient
 from collections import OrderedDict, defaultdict
+from pprint import pprint
 
 f = open("shorashim.torah.out.txt", 'r', encoding='utf-8')
 pasuk_dict = dict()
@@ -10,13 +11,16 @@ for line in f:
     pasuk_dict[reference] = eval(data)
 f.close()
 
+p = OrderedDict()
+parsha_text = dict()
+parsha_refs = dict()
+
 def createParshiot():
     # connect to the db server
     client = MongoClient()
     db = client.sefaria
 
     parshiot = db.parshiot
-    p = OrderedDict()
     stop_list = "Yom Sukkot Pesach Rosh Shabbat Atzeret Shavuot -".split()
     for parsha in parshiot.find():
         name = parsha['parasha']
@@ -36,7 +40,8 @@ def createParshiot():
     # using the starting chapter and verse and ending chapter and verse, iterate through
     # and use it to look up in the ETCB dataset
     text = []
-    parsha_text = dict()
+    refs = []
+
     for parsha, t in p.items():
         print(parsha)
         sefer, start_ch, start_v, end_ch, end_v = t
@@ -44,7 +49,8 @@ def createParshiot():
         for verse in range(start_v, 1000):
             ref = sefer + ' ' + str(start_ch) + ' ' + str(verse)
             if ref in pasuk_dict:
-                text += [root for _, root in pasuk_dict[ref]]
+                text += [root for _, root in pasuk_dict[ref]] + [':']
+                refs.append(ref)
             else: # out of pesukim
                 break
 
@@ -53,7 +59,8 @@ def createParshiot():
             for verse in range(1, end_v+1):
                 ref = sefer + ' ' + str(chapter) + ' ' + str(verse)
                 if ref in pasuk_dict:
-                    text += [root for _, root in pasuk_dict[ref]]
+                    text += [root for _, root in pasuk_dict[ref]] + [':']
+                    refs.append(ref)
                 else:  # out of pesukim
                     break
 
@@ -62,12 +69,15 @@ def createParshiot():
             for verse in range(1, 1000):
                 ref = sefer + ' ' + str(end_ch) + ' ' + str(verse)
                 if ref in pasuk_dict:
-                    text += [root for _, root in pasuk_dict[ref]]
+                    text += [root for _, root in pasuk_dict[ref]] + [':']
+                    refs.append(ref)
                 else:  # out of pesukim
                     break
 
-        parsha_text[parsha] = text
+        parsha_text[parsha] = ' '.join(text)
+        parsha_refs[parsha] = refs
         text = []
+        refs = []
         #break
 
     pnames = ["Bereshit", "Noach", "Lech-Lecha", "Vayera", "Chayei Sara", "Toldot", "Vayetzei", "Vayishlach", "Vayeshev", "Miketz", "Vayigash", "Vayechi", "Shemot", "Vaera", "Bo", "Beshalach", "Yitro", "Mishpatim", "Terumah", "Tetzaveh", "Ki Tisa", "Vayakhel", "Pekudei", "Vayikra", "Tzav", "Shmini", "Tazria", "Metzora", "Achrei Mot", "Kedoshim", "Emor", "Behar", "Bechukotai", "Bamidbar", "Nasso", "Beha'alotcha", "Sh'lach", "Korach", "Chukat", "Balak", "Pinchas", "Matot", "Masei", "Devarim", "Vaetchanan", "Eikev", "Re'eh", "Shoftim", "Ki Teitzei", "Ki Tavo", "Nitzavim", "Vayeilech", "Ha'Azinu", "VeZot HaBeracha"]
@@ -77,9 +87,13 @@ def createParshiot():
     f.close()
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+tf_parsha = {}
 def gather_tf_idf():
    f = open('parshiot.txt', 'r', encoding='utf-8')
-   docs = [line for line in f]
+   docs = []
+   for line in f:
+       line = line.replace(' : ', ' ')
+       docs.append(line)
    f.close()
 
    vectorizer = TfidfVectorizer(analyzer="word",token_pattern="[\S]+")
@@ -88,14 +102,42 @@ def gather_tf_idf():
    vocab_lookup2 = {k: v for k, v in vectorizer.vocabulary_.items()}
 
    feature_names = vectorizer.get_feature_names()
+   pnames = ["Bereshit", "Noach", "Lech-Lecha", "Vayera", "Chayei Sara", "Toldot", "Vayetzei", "Vayishlach", "Vayeshev",
+             "Miketz", "Vayigash", "Vayechi", "Shemot", "Vaera", "Bo", "Beshalach", "Yitro", "Mishpatim", "Terumah",
+             "Tetzaveh", "Ki Tisa", "Vayakhel", "Pekudei", "Vayikra", "Tzav", "Shmini", "Tazria", "Metzora",
+             "Achrei Mot", "Kedoshim", "Emor", "Behar", "Bechukotai", "Bamidbar", "Nasso", "Beha'alotcha", "Sh'lach",
+             "Korach", "Chukat", "Balak", "Pinchas", "Matot", "Masei", "Devarim", "Vaetchanan", "Eikev", "Re'eh",
+             "Shoftim", "Ki Teitzei", "Ki Tavo", "Nitzavim", "Vayeilech", "Ha'Azinu", "VeZot HaBeracha"]
 
-   doc = 0
-   feature_index = X[doc, :].nonzero()[1]
-   tfidf_scores = zip(feature_index, [X[doc, x] for x in feature_index])
+   for doc, name in enumerate(pnames):
+       feature_index = X[doc, :].nonzero()[1]
+       tfidf_scores = zip(feature_index, [X[doc, x] for x in feature_index])
 
-   d = {}
-   for w, s in [(feature_names[i], s) for (i, s) in tfidf_scores]:
-       d[w] = s
+       d = {}
+       for w, s in [(feature_names[i], s) for (i, s) in tfidf_scores]:
+           d[w] = s
+
+       tf_parsha[name] = d
+
+   pprint(tf_parsha)
+
+def find_leitworte():
+    parsha = 'Bereshit' # for sample while dev
+
+    VERSE_WINDOW = 20
+    REPETITION = 7
+    verses = parsha_text[parsha].split(' : ')
+    for verse in verses:
+        print(verse)
+        break
+
+    bagOfWordsSpan = dict()
+    bagOfWordsCurrent = dict()
+    for verse in verses:
+        pass
+
+    print('moo')
+    # todo finish leitwort detection
 
 # step 1: create the parshiot.txt, helpful for tf-idf
 # comment out if you already have parshiot.txt
@@ -104,4 +146,5 @@ createParshiot()
 #step 2: get the tf_idf scores for all words in all documents
 gather_tf_idf()
 
-
+# step 3: find leitworte, for each parasha, moving thru window of verses
+find_leitworte()
